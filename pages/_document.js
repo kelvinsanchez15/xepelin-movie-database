f7978b1d48ed4d59c0928c937cb8a957615865bc
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-10-14 16:05:12 -0500

    Add custom google font

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           />
         </Head>
         <body>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,9 @@ import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
+  typography: {
+    fontFamily: ["Space Grotesk", "sans-serif"].join(","),
+  },
   palette: {
     primary: {
       main: "#556cd6",
