5ae203227eacfd8e0f51b21c08d703cc164e3aff
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-10-15 08:22:41 -0500

    Create api functions to query movies

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import formatDateLocally from "../utils/formatDateLocally";
+
+const BASE_URL = "https://api.themoviedb.org/3/movie";
+const API_KEY = "b69d9e5ccb7d627bc02f3d704e5ca529";
+const LANGUAGE = "es-MX";
+
+const normalizeResults = (results) => {
+  return results.map((result) => {
+    const {
+      id,
+      title,
+      release_date: releaseDate,
+      backdrop_path: backdropPath,
+      overview,
+    } = result;
+
+    return {
+      id,
+      title,
+      releaseDate: formatDateLocally(releaseDate),
+      backdropPath,
+      overview,
+    };
+  });
+};
+
+export const getPopularMovies = async () => {
+  const query = `${BASE_URL}/popular?api_key=${API_KEY}&language=${LANGUAGE}&page=1`;
+  const res = await fetch(query);
+  const { results } = await res.json();
+  return normalizeResults(results);
+};
+
+export const getTopRatedMovies = async () => {
+  const query = `${BASE_URL}/top_rated?api_key=${API_KEY}&language=${LANGUAGE}&page=1`;
+  const res = await fetch(query);
+  const { results } = await res.json();
+  return normalizeResults(results);
+};
+
+export const getUpcomingMovies = async () => {
+  const query = `${BASE_URL}/upcoming?api_key=${API_KEY}&language=${LANGUAGE}&page=1`;
+  const res = await fetch(query);
+  const { results } = await res.json();
+  return normalizeResults(results);
+};
+
+export const getLatestMovies = async () => {
+  const query = `${BASE_URL}/now_playing?api_key=${API_KEY}&language=${LANGUAGE}&page=1`;
+  const res = await fetch(query);
+  const { results } = await res.json();
+  return normalizeResults(results);
+};
