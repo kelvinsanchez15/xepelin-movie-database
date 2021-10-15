b656df8d536555c0f4edb961b72a6e224cf635d9
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-10-15 08:20:46 -0500

    Change theme primary color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,12 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
 
-// Create a theme instance.
 const theme = createTheme({
   typography: {
     fontFamily: ["Space Grotesk", "sans-serif"].join(","),
   },
   palette: {
     primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
+      main: "#5031A9",
     },
   },
   breakpoints: {
