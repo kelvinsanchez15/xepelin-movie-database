e41771c145dd62a10bb41baf713ab4429d9321dd
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-10-15 08:26:55 -0500

    Update head title

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,10 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>Xepelin Movie Database</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
