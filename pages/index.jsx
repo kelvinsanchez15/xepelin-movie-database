a95de8fce6fe6bbf097f977f6f2d34da976ca48d
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-10-17 09:02:01 -0500

    Render user favorite movies

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,22 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Container, Box, Grid, Typography } from "@mui/material";
 
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 import Hero from "../src/components/Hero";
+import MoviesCarousel from "../src/components/MoviesCarousel";
 
-import "react-multi-carousel/lib/styles.css";
 import {
   getLatestMovies,
   getPopularMovies,
   getTopRatedMovies,
   getUpcomingMovies,
+  getFavoritesMoviesWithDetails,
 } from "../src/api";
-import MoviesCarousel from "../src/components/MoviesCarousel";
+import { useGetFavoritesMoviesMutation } from "../src/app/services/auth";
+import { selectFavoritesMovies } from "../src/app/favoritesMoviesSlice";
+import { useAuth } from "../src/hooks/useAuth";
 
 export default function Index({
   popularMovies,
@@ -19,11 +24,66 @@ export default function Index({
   upcomingMovies,
   latestMovies,
 }) {
+  const { user } = useAuth();
+  const [getFavoritesMovies, { isLoading }] = useGetFavoritesMoviesMutation();
+  const [favoritesMoviesWithDetails, setFavoriteMoviesWithDetails] = useState(
+    []
+  );
+  const [loadingFavoritesMovies, setLoadingFavoritesMovies] = useState(false);
+
+  const favoritesMovies = useSelector(selectFavoritesMovies);
+
+  useEffect(() => {
+    if (user) {
+      setLoadingFavoritesMovies(true);
+      getFavoritesMovies();
+    }
+  }, [user, getFavoritesMovies]);
+
+  useEffect(() => {
+    if (favoritesMovies.length >= 1) {
+      (async () => {
+        const movies = await getFavoritesMoviesWithDetails(favoritesMovies);
+        setFavoriteMoviesWithDetails(movies);
+        setLoadingFavoritesMovies(false);
+      })();
+    }
+    if (favoritesMovies.length === 0 && !isLoading) {
+      setLoadingFavoritesMovies(false);
+    }
+  }, [favoritesMovies, setFavoriteMoviesWithDetails, isLoading]);
+
   return (
     <>
       <Hero />
       <Container>
         <Box sx={{ py: 12 }}>
+          {/* Peliculas favoritas */}
+          {user && (
+            <Box id="favorites-movies" sx={{ mb: 8 }}>
+              <Grid container direction="row" alignItems="center" spacing={1}>
+                <Grid item>
+                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                    Favoritas
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <ArrowForwardIcon sx={{ display: "flex" }} />
+                </Grid>
+              </Grid>
+              <Typography component="p" sx={{ color: "text.secondary", mb: 4 }}>
+                Donec eu lobortis
+              </Typography>
+
+              {favoritesMoviesWithDetails && (
+                <MoviesCarousel
+                  movies={favoritesMoviesWithDetails}
+                  loading={loadingFavoritesMovies || isLoading}
+                />
+              )}
+            </Box>
+          )}
+
           {/* Peliculas más populares */}
           <Box id="popular-movies" sx={{ mb: 8 }}>
             <Grid container direction="row" alignItems="center" spacing={1}>
